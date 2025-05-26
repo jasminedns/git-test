c9cb3ad6f7d4fc2239d974b8b6010dba0cb47345
Jasminedns <jasminedanese@icloud.com>
2025-05-26 16:08:15 +0200

    added h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>GIT test</h1>
+    <>
+      <h1>GIT test</h1>
+      <h2>ALWAYS create a new branch from MAIN</h2>
+    </>
   );
 }
